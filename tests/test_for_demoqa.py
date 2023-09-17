df70b494bcaa243597991c432df1bd850daf9ddf
natal <svetnata74@mail.ru>
2023-09-17 10:47:47 +0200

    yhjg

--- tests/test_for_demoqa.py
+++ tests/test_for_demoqa.py
@@ -5,7 +5,7 @@ from selene import browser
 def test_registration_page():
     # GIVEN
 
-    browser.open("https://demoqa.com/automation-practice-form")
+    browser.open('https://demoqa.com/automation-practice-form')
     browser.execute_script('document.querySelector("#fixedban").remove()')
     browser.element('footer').execute_script('element.remove()')
 
