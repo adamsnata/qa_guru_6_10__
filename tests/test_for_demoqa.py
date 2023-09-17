2801d1b474d62a969ba966ca2a59848ac91175a3
natal <svetnata74@mail.ru>
2023-09-17 10:46:01 +0200

    Initial commit

---
+++ tests/test_for_demoqa.py
@@ -0,0 +1,12 @@
+from selene import browser
+
+
+
+def test_registration_page():
+    # GIVEN
+
+    browser.open("https://demoqa.com/automation-practice-form")
+    browser.execute_script('document.querySelector("#fixedban").remove()')
+    browser.element('footer').execute_script('element.remove()')
+
+
